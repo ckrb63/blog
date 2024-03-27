23a7e35218afae29823cb5a321a540fd74f747e7
cksrb63 <cksr1@naver.com>
2024-03-27 12:07:41 +0900

    Fix: summary

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -54,7 +54,7 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
             </div>
           </header>
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
-            <dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700"></dl>
+            <dl className="pb-10 pt-6 xl:pt-0 xl:pb-0 xl:border-gray-200 xl:dark:border-gray-700"></dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
             </div>
